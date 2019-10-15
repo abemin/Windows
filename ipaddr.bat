ac901bcb7efc36ec784c59690b806fc256ed1a22
abemin <muhaimifatihi@hotmail.com>
2019-10-15 22:05:03 +0800

    Update ipaddr.bat
    adding comment and renaming interface

--- ipaddr.bat
+++ ipaddr.bat
@@ -2,6 +2,7 @@
 cls
 :start
 ECHO.
+:: Options for user to choose
 ECHO 1. Change Ethernet Static IP 
 ECHO 2. Obtain an IP address automatically
 ECHO 3. Exit
@@ -16,14 +17,16 @@ ECHO.
 goto start
 :con1
 ECHO Connecting Ethernet
-netsh interface ip set address "Ethernet 2" static 172.22.1.94 255.255.255.128 172.22.1.3
-netsh interface ip set dns "Ethernet 2" static 192.168.240.18
+:: Change interface_name to your interface and IP to your static IP
+netsh interface ip set address "interface_name" static 10.10.100.99 255.255.255.128 10.10.100.2
+netsh interface ip set dns "Interface_name" static 192.168.0.20
 goto end
 
 :autosearch
 ECHO obtaining auto IP
-netsh interface ip set address "Ethernet 2" dhcp
-netsh interface ip set dns "Ethernet 2" dhcp
+:: Change interface_name to your interface
+netsh interface ip set address "interface_name" dhcp
+netsh interface ip set dns "interface_name" dhcp
 goto end
 
 :bye
